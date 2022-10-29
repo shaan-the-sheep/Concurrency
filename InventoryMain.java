ba5955a771e2248e9c259bc686472714bd8c6693
Jassal <jassals@vdi-scclab-011.lancs.local>
2022-10-29 17:04:10 +0100

    Interleaved the threads
    Signed-off-by: Jassal <jassals@vdi-scclab-011.lancs.local>

--- InventoryMain.java
+++ InventoryMain.java
@@ -15,9 +15,25 @@ public class InventoryMain {
                 }
                 InventoryTracking IT = new InventoryTracking();
                 Warehouse w = new Warehouse(IT, bugFlag);
-                w.doAllAdds(numOfAdd); // completing all add operations
-                w.doAllRemoves(numOfRemove);// completing all remove operations
-                
+                int temp = 0;
+                if (numOfAdd >= numOfRemove){
+                    for(int i=0; i<numOfAdd; i++){
+                        w.addItem();
+                        if (temp < numOfRemove){
+                            w.removeItem();
+                            temp++;
+                        }
+                    }
+                }
+                else{
+                    for(int i=0; i<numOfRemove; i++){
+                        w.removeItem();
+                        if (temp < numOfAdd){
+                            w.addItem();
+                            temp++;
+                        }
+                    }
+                }
             } catch (NumberFormatException e) {
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
--- Warehouse.java
+++ Warehouse.java
@@ -7,18 +7,15 @@ public class Warehouse {
         this.bugFlag = bugFlag;
     }
 
-    public void doAllAdds(int numOfAdd){
-        for(int i = 0; i<numOfAdd; i++){
-            AddToWarehouse a = new AddToWarehouse(IT, bugFlag);
-            a.start();
-        }
+    public void addItem(){
+        AddToWarehouse a = new AddToWarehouse(IT, bugFlag);
+        a.start();
     }
+    
 
-    public void doAllRemoves(int numOfRemove){
-        for(int i = 0; i<numOfRemove; i++){
-            RemoveFromWarehouse r = new RemoveFromWarehouse(IT, bugFlag);
-            r.start();
-        }
+    public void removeItem(){
+        RemoveFromWarehouse r = new RemoveFromWarehouse(IT, bugFlag);
+        r.start();
     }
 }
 
